41c53dbd4c1d09df420dfe5860d7070913527e1a
Ambuj-Pandey <ambujpandey4894@gmail.com>
2024-04-30 23:58:48 +0530

    wp-book custom dashboard widget

--- wp-book/wp-book.php
+++ wp-book/wp-book.php
@@ -21,3 +21,4 @@ register_activation_hook( __FILE__, 'wp_book_create_meta_table' );
 
 include_once WP_BOOK_PLUGIN_DIR . 'includes/wp-book-admin-settings.php';
 include_once WP_BOOK_PLUGIN_DIR . 'includes/wp-book-shortcode.php';
+include_once WP_BOOK_PLUGIN_DIR . 'includes/wp-book-custom-dashboard-widget.php';
